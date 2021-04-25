65c18a55da6a7e180115241f7f1974be1257604b
Iasonas Pavlidis <iasonaspav@gmail.com>
2021-04-25 21:16:44 +0300

    refs #1 Use immediately blocks of Q without intermediate variables to ensure orthogonality

--- RBL.m
+++ RBL.m
@@ -15,8 +15,8 @@ function [V,D] = RBL(A,k,b)
     n = size(A,2);
     T = zeros(k*b,k*b);
     Q = zeros(n,k*b);
-    Q0 = randn(n,b);
-    [Q1,~] = qr(A*Q0,0);
+    Q1 = randn(n,b);
+    [Q1,~] = qr(A*Q1,0);
    
     % first loop
     Q(:,1:b) = Q1;
@@ -29,20 +29,20 @@ function [V,D] = RBL(A,k,b)
     T(b+1:2*b,1:b) = B;
     T(1:b,b+1:2*b) = B.';
     i = 2;
-    while true
+    while i*b < 200
         Q(:,(i-1)*b+1:i*b) = Q1;
-        [Q(:,(i-2)*b+1:i*b),~] = qr( Q(:,(i-2)*b+1:i*b),0 );
-        if mod(i,2) == 0   
-            [Q(:,1:i*b),~] = qr( Q(:,1:i*b),0 );
+        %[Q(:,(i-2)*b+1:i*b),~] = qr( Q(:,(i-2)*b+1:i*b),0 );
+        if mod(i,4) == 0
+            %[Q(:,1:i*b),~] = qr( Q(:,1:i*b),0 );
         end
-        U = A*Q1 - Q0*B.';
-        M = Q1.'*U;
-        R = U - Q1*M;
-        Q0 = Q1;
+        U = A*Q(:,(i-1)*b+1:i*b) - Q(:,(i-2)*b+1:(i-1)*b)*B.';
+        M = Q(:,(i-1)*b+1:i*b).'*U;
+        R = U - Q(:,(i-1)*b+1:i*b)*M;
+        %Q0 = Q(:,(i-1)*b+1:i*b);
         [Q1,B] = qr(R,0);
         T((i-1)*b+1:i*b,(i-1)*b+1:i*b) = M;
         if i > k
-        [V,D,~] = svd(T);
+            [V,D,~] = svd(T);
             if norm(B*V(end-b+1:end,k)) < 1.0e-2
                 break;
             end
