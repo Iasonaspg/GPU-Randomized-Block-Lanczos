9fa36c2d0ed472869321d0acdfb8d6b36d2b2457
iasonas <iasonaspav@gmail.com>
2021-08-16 23:54:54 +0300

    refs #5 Remove timing measurements from GPU implementation

--- Julia/RBL_gpu.jl
+++ Julia/RBL_gpu.jl
@@ -20,35 +20,35 @@ end
 # orthogonalize the two latest blocks against all the previous
 function part_reorth_gpu!(U::Vector{Matrix{FLOAT}})
     i = size(U,1);
-    @timeit to "CuArray allocation" U1 = CuArray{FLOAT}(U[i]);
-    @timeit to "CuArray allocation" U2 = CuArray{FLOAT}(U[i-1]);
+    U1 = CuArray{FLOAT}(U[i]);
+    U2 = CuArray{FLOAT}(U[i-1]);
     Uj = CUDA.zeros(size(U1,1),size(U1,2));
     # Uj_T = CUDA.zeros(size(U1,2),size(U1,1));
     for j=1:i-2
-        @timeit to "Load Uj" copyto!(Uj,U[j]);
+        copyto!(Uj,U[j]);
         # @timeit to "transpose" transpose!(Uj_T,Uj);
-        @timeit to "Uj^T*U1" temp = transpose(Uj)*U1;
-        @timeit to "U1" U1 = U1 - Uj*temp;
-        @timeit to "Uj^T*U2" temp = transpose(Uj)*U2;
-        @timeit to "U2" U2 = U2 - Uj*temp;
+        temp = transpose(Uj)*U1;
+        U1 = U1 - Uj*temp;
+        temp = transpose(Uj)*U2;
+        U2 = U2 - Uj*temp;
     end
-    @timeit to "copy to CPU" copyto!(U[i],U1);
-    @timeit to "copy to CPU" copyto!(U[i-1],U2);
+    copyto!(U[i],U1);
+    copyto!(U[i-1],U2);
     return nothing
 end
 
 function part_reorth_gpu_block!(U1::CuArray{FLOAT},U2::CuArray{FLOAT},Ug::CuArray{FLOAT})
-    @timeit to "Uj^T*U1" temp = transpose(Ug)*U1;
-    @timeit to "U1" U1[:,:] = U1 - Ug*temp;
-    @timeit to "Uj^T*U2" temp = transpose(Ug)*U2;
-    @timeit to "U2" U2[:,:] = U2 - Ug*temp;
+    temp = transpose(Ug)*U1;
+    U1[:,:] = U1 - Ug*temp;
+    temp = transpose(Ug)*U2;
+    U2[:,:] = U2 - Ug*temp;
     return nothing
 end
 
 function loc_reorth_gpu!(U1::CuArray{FLOAT},U2::CuArray{FLOAT})
-    @timeit to "transpose" temp = transpose(U2)*U1;
-    @timeit to "u1-u2*temp" temp = U1 - U2*temp;
-    @timeit to "qr" U1[:,:] = CuArray(qr(temp).Q);
+    temp = transpose(U2)*U1;
+    temp = U1 - U2*temp;
+    U1[:,:] = CuArray(qr(temp).Q);
     return nothing;
 end
 
@@ -56,7 +56,7 @@ function RBL_gpu(A::SparseMatrixCSC{FLOAT},k::Int64,b::Int64)
     n = size(A,2);
     V = zeros(FLOAT);
     D = zeros(FLOAT);
-    @timeit to "A gpu allocation" Ag = adapt(CuArray, A);
+    Ag = adapt(CuArray, A);
     
     Qi = randn(FLOAT,n,b);
     Qg = CuArray(Qi);
@@ -72,12 +72,12 @@ function RBL_gpu(A::SparseMatrixCSC{FLOAT},k::Int64,b::Int64)
     avail_mem = avail_mem - 11*bl_sz - sparse_size(A);
     buffer_size::Int32 = floor(avail_mem/bl_sz);
     println("buffer_size: $buffer_size");
-    buffer_size = 45;
+    # buffer_size = 1;
 
     # first loop
     push!(Q,Array(Qg));
     push!(Qgpu,copy(Qg));
-    @timeit to "A*Qi" U = Ag*Qg;
+    U = Ag*Qg;
     Ai = transpose(Qg)*U;
     R = U - Qg*Ai;
     fact = qr(R);
@@ -90,7 +90,6 @@ function RBL_gpu(A::SparseMatrixCSC{FLOAT},k::Int64,b::Int64)
     while i*b < 1000
         if mod(i,3) == 0
             if i > buffer_size
-                @timeit to "part_reorth_hybrid" begin
                     last = min(buffer_size,i-2);
                     for j=1:last
                         part_reorth_gpu_block!(Qg,Qg1,Qgpu[j]);
@@ -98,30 +97,27 @@ function RBL_gpu(A::SparseMatrixCSC{FLOAT},k::Int64,b::Int64)
                     for j=last+1:i-2
                         copyto!(Qgj,Q[j]);
                         part_reorth_gpu_block!(Qg,Qg1,Qgj);
-                    end
                 end
             else
-                @timeit to "part_reorth_on" begin
                     for j=1:i-2
                         part_reorth_gpu_block!(Qg,Qg1,Qgpu[j]);
-                    end
                 end
                 copyto!(Qgpu[i-1],Qg1);
             end
             copyto!(Q[i-1],Qg1);
         end
-        @timeit to "loc_reorth" loc_reorth_gpu!(Qg,Qg1);
+        loc_reorth_gpu!(Qg,Qg1);
         push!(Q,Array(Qg));
         if i <= buffer_size
             push!(Qgpu,copy(Qg));
         end
         Big = CuArray{FLOAT}(Bi);
-        @timeit to "A*Qi" U = Ag*Qg - Qg1*transpose(Big);
-        @timeit to "Ai" Ai = transpose(Qg)*U;
-        @timeit to "U-QiAi" R = U - Qg*Ai;
-        @timeit to "qr" fact = qr(R);
+        U = Ag*Qg - Qg1*transpose(Big);
+        Ai = transpose(Qg)*U;
+        R = U - Qg*Ai;
+        fact = qr(R);
         copyto!(Qg1,Qg);
-        @timeit to "qr" Qg = CuArray(fact.Q);
+        Qg = CuArray(fact.Q);
         Bi = Array(fact.R);
         T = [T insertA!(Array(Ai),b)];
         if i*b > k
