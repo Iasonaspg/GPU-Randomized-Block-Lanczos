e1a60993a9c9249a080c2fa724e8505702f4e6fe
iasonas <iasonaspav@gmail.com>
2021-12-16 23:42:13 +0200

    refs #8 Create new benchmark julia script

--- Julia/benchmark.jl
+++ Julia/benchmark.jl
@@ -1,24 +1,40 @@
-using LinearAlgebra
-using BenchmarkTools
-using ProfileView
 using TimerOutputs
-include("./RBL.jl")
+using MAT
+using MatrixMarket
+# using Arpack
+# using MKL
 
-const to = TimerOutput();
+include("./RBL.jl")
+include("./RBL_gpu.jl")
 
 function bench()
-    BLAS.set_num_threads(6)
-    file = matopen("C:/Users/Iasonas/Desktop/Git Repositories/randomized-block-lanczos/matrices/Serena.mat");
-    problem = read(file,"Problem");
-    A = problem["A"];
-    println("Lets start!");
+    # file = matopen("/home/iasonas/Desktop/randomized-block-lanczos/Matrix/ldoor.mat")
+    # Problem = read(file,"Problem");
+    # A::SparseMatrixCSC{DOUBLE} = Problem["A"];
+    A::SparseMatrixCSC{DOUBLE} = mmread("/home/iasonas/Desktop/randomized-block-lanczos/Matrix/hood.mtx");
 
-    @timeit to "RBL" D,_ = RBL_timed(A,50,4); 
-    # @time D,_ = RBL(A,50,4); 
-    # @time D,_ = RBL(A,50,4); 
-    # @time D,_ = RBL(A,50,4); 
-    println(D);
+    @timeit to "RBL" @time d,v = RBL_restarted(A,100,2,10);
+    # @timeit to "RBL" @time d,v = RBL(A,100,4);
+    # @timeit to "RBL_gpu" CUDA.@time d,_ = RBL_gpu(A,50,1);
+    # @timeit to "RBL_gpu" CUDA.@profile d,_ = RBL_gpu(A,50,1);
+    # println(d);
+    # println("\n\n");
+
+    # @timeit to "Arpack" @time d,v = eigs(A,nev=10);
+    # println(d);
+    # res = norm(A*v[:,10] - d[10]*v[:,10]);
+    # println("Norm: $res");
+    # println("\n\n");
 end
 
+BLAS.set_num_threads(1);
+# CUDA.math_mode!(CUDA.DEFAULT_MATH);
+
+to = TimerOutput();
+d,_ = RBL(sprandn(DOUBLE,200,200,0.5),1,1);
+# d,_ = RBL_gpu(sprandn(DOUBLE,50,50,0.5),1,10);
+to = TimerOutput();
 bench();
-show(to)
+show(to);
+println();
+println("\n\n");
