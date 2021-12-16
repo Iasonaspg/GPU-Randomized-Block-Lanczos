d284f1bf1e852d008420e795c11ab2937e99a2b1
iasonas <iasonaspav@gmail.com>
2021-12-16 23:45:21 +0200

    refs #8 Implement a naive restarted RBL based on the thick-restart method

--- Julia/RBL.jl
+++ Julia/RBL.jl
@@ -1,60 +1,32 @@
-using LinearAlgebra
-using MAT
-using TimerOutputs
-
-include("RBL_gpu.jl")
-
-# Require square A
-function insertA!(A::Matrix{DOUBLE}, b::Int64)
-    T = zeros(DOUBLE,b+1,b);
-    n = size(A,2);
-    for j = 1:n
-        size = n - j + 1;
-        T[1:size,j] = A[j:n,j];
-    end
-    return T;
-end
-
-# Require square B
-function insertB!(B::Matrix{DOUBLE}, T::Matrix{DOUBLE}, b::Int64, iter::Int64)
-    n = size(B,2);
-    start = (iter-1)*b;
-    for j = 1:n
-        T[end-j+1:end,start+j] = B[1:j,j];
-    end
-end
-
-function dsbev_lapack(jobz::Char, uplo::Char, n::Int64, bw::Int64, a::Matrix{Float32}, lda::Int64, D::Matrix{Float32}, V::Matrix{Float32}, ldz::Int64, work::Vector{Float32}, info::Int64)
-    ccall((:ssbev_64_, Base.liblapack_name), Nothing, (Ref{UInt8}, Ref{UInt8}, Ref{Int64}, Ref{Int64}, Ptr{Float32}, Ref{Int64}, Ptr{Float32}, Ptr{Float32}, Ref{Int64}, Ptr{Float32}, Ref{Int64}), jobz, uplo, n, bw, a, lda, D, V, ldz, work, info)
-end
-
-function dsbev_lapack(jobz::Char, uplo::Char, n::Int64, bw::Int64, a::Matrix{Float64}, lda::Int64, D::Matrix{Float64}, V::Matrix{Float64}, ldz::Int64, work::Vector{Float64}, info::Int64)
-    ccall((:dsbev_64_, Base.liblapack_name), Nothing, (Ref{UInt8}, Ref{UInt8}, Ref{Int64}, Ref{Int64}, Ptr{Float64}, Ref{Int64}, Ptr{Float64}, Ptr{Float64}, Ref{Int64}, Ptr{Float64}, Ref{Int64}), jobz, uplo, n, bw, a, lda, D, V, ldz, work, info)
-end
-
-function dsbev(jobz::Char, uplo::Char, A::Matrix{DOUBLE})
-    n = size(A,2);
-    lda = size(A,1);
-    bw = lda - 1;
-    ldz = n;
-    a = copy(A);
-    work = zeros(DOUBLE,3*n-2);
-    info = 0;
-    D = zeros(DOUBLE,n,1);
-    V = zeros(DOUBLE,ldz,n);
-    dsbev_lapack(jobz,uplo,n,bw,a,lda,D,V,ldz,work,info);
-    return D,V;
-end
+include("common.jl")
 
 # orthogonalize U1 against U2
 function loc_reorth!(U1::Matrix{FLOAT},U2::Matrix{FLOAT})
-    temp = BLAS.gemm('T','N',FLOAT(1.0),U2,U1);
-    BLAS.gemm!('N','N',FLOAT(-1.0),U2,temp,FLOAT(1.0),U1);
-    U1[:,:] = Matrix(qr(U1).Q);
+    p = size(U1,2);
+    for i=1:2*p
+        temp = BLAS.gemm('T','N',FLOAT(1.0),U2,U1);
+        BLAS.gemm!('N','N',FLOAT(-1.0),U2,temp,FLOAT(1.0),U1);
+        U1 = Matrix(qr(U1).Q);
+    end
+    U1[:,:] = U1;
     return nothing;
 end
 
 # orthogonalize the two latest blocks against all the previous
+function part_reorth!(nblocks::Int64,U::Vector{Matrix{FLOAT}},U1::Matrix{FLOAT},U2::Matrix{FLOAT})
+    temp = Array{FLOAT}(undef,size(U1,2),size(U1,2));
+    for j=1:nblocks
+        Uj = U[j];
+        @timeit to "Uj_t*U1" mul!(temp,transpose(Uj),U1);
+        @timeit to "U1" mul!(U1,Uj,temp,FLOAT(-1.0),FLOAT(1.0)); # FLOAT is needed to prevent cast to Float64 and copy of data!
+        @timeit to "Uj_t*U2" mul!(temp,transpose(Uj),U2);
+        @timeit to "U2" mul!(U2,Uj,temp,FLOAT(-1.0),FLOAT(1.0));
+    end
+    U1[:,:] = U1;
+    U2[:,:] = U2;
+    return nothing
+end
+
 function part_reorth!(U::Vector{Matrix{FLOAT}})
     i = size(U,1);
     temp = Array{FLOAT}(undef,size(U[1],2),size(U[1],2));
@@ -64,6 +36,17 @@ function part_reorth!(U::Vector{Matrix{FLOAT}})
         @timeit to "U1" mul!(U[i],Uj,temp,FLOAT(-1.0),FLOAT(1.0));
         @timeit to "Uj_t*U2" mul!(temp,transpose(Uj),U[i-1]);
         @timeit to "U2" mul!(U[i-1],Uj,temp,FLOAT(-1.0),FLOAT(1.0));
+    end
+    return nothing
+end
+
+function restart_reorth!(U::Vector{Matrix{FLOAT}},Q::Matrix{FLOAT})
+    len = length(U);
+    temp = Array{FLOAT}(undef,size(Q,2),size(Q,2));
+    for j=1:len
+        Uj = U[j];
+        @timeit to "Uj_t*U1" mul!(temp,transpose(Uj),Q);
+        @timeit to "U1" mul!(Q,Uj,temp,FLOAT(-1.0),FLOAT(1.0));
     end
     return nothing
 end
@@ -80,29 +63,13 @@ function recover_eigvec(Q::Vector{Matrix{FLOAT}},V_trunc::Matrix{FLOAT},k::Int64
     return V;
 end
 
-function RBL(A::Union{SparseMatrixCSC{DOUBLE}},k::Int64,b::Int64)
-#=
-Input parameters
-A - n by n Real Symmetric Matrix whose eigenvalues we seek
-k - number of largest eigenvalues
-b - block size
 
-Output parameters
-D - k by 1 vector with k largest eigenvalues of A (by magnitude)
-V - n by k matrix with eigenvectors associated with the k largest eigenvalues of A
-
-This routine uses the randomized block Lanczos algorithm to compute the k 
-largest eigenvalues of a matrix A.
-=#
-
-    n = size(A,2);
-    Q = Matrix{FLOAT}[];
-    Qi = randn(DOUBLE,n,b);
-    Qi = Matrix{FLOAT}(qr(A*Qi).Q);
-    V = zeros(DOUBLE);
+function lanczos_iteration(A::SparseMatrixCSC{DOUBLE},k::Int64,b::Int64,kryl_sz::Int64,Qi::Matrix{FLOAT},Q::Vector{Matrix{FLOAT}},Qlock::Vector{Matrix{FLOAT}})
     D = zeros(DOUBLE);
-   
+    V = zeros(DOUBLE);
+    
     # first loop
+    restart_reorth!(Qlock,Qi);
     push!(Q,Qi);
     @timeit to "A*Qi" U::Matrix{DOUBLE} = A*Qi;
     Ai::Matrix{DOUBLE} = transpose(Qi)*U;
@@ -114,9 +81,10 @@ largest eigenvalues of a matrix A.
     T = insertA!(Ai,b);
     insertB!(Bi,T,b,1);
     i = 2;
-    while i*b < 1000
+    while i*b < kryl_sz
         push!(Q,Qi);
         if mod(i,3) == 0
+            part_reorth!(length(Qlock),Qlock,Q[i],Q[i-1]);
             @timeit to "part_reorth" part_reorth!(Q);
         end
         @timeit to "loc_reorth" loc_reorth!(Q[i],Q[i-1]);
@@ -131,7 +99,7 @@ largest eigenvalues of a matrix A.
         T = [T insertA!(Ai,b)];
         if i*b > k
             @timeit to "dsbev" D,V = dsbev('V','L',T);
-            if norm(Bi*V[end-b+1:end,end-k+1]) < 1.0e-10
+            if norm(Bi*V[end-b+1:end,end-k+1]) < 1.0e-5
                break;
             end
         end
@@ -139,27 +107,88 @@ largest eigenvalues of a matrix A.
         i = i + 1;
     end
     println("Iterations: $i");
-    D = D[end:-1:end-k+1];
-    @timeit to "Recovery" V = recover_eigvec(Q,Matrix{FLOAT}(V[:,end:-1:end-k+1]),k);  # V = Q*V(:,1:k);
     return D,V;
 end
 
-function bench()
-    file = matopen("/home/iasonas/Desktop/randomized-block-lanczos/Matrix/hood.mat")
-    Problem = read(file,"Problem");
-    A::SparseMatrixCSC{DOUBLE} = Problem["A"];
-    # @timeit to "RBL" @time d,_ = RBL(A,25,10);
-    @timeit to "RBL_gpu" CUDA.@time d,_ = RBL_gpu(A,25,10);
-    # @timeit to "RBL_gpu" CUDA.@profile d,_ = RBL_gpu(A,25,10);
-    println(d);
+
+function RBL(A::Union{SparseMatrixCSC{DOUBLE}},k::Int64,b::Int64)
+#=
+Input parameters
+A - n by n Real Symmetric Matrix whose eigenvalues we seek
+k - number of largest eigenvalues
+b - block size
+
+Output parameters
+D - k by 1 vector with k largest eigenvalues of A (by magnitude)
+V - n by k matrix with eigenvectors associated with the k largest eigenvalues of A
+
+This routine uses the randomized block Lanczos algorithm to compute the k 
+largest eigenvalues of a matrix A.
+=#
+    max_kryl_sz = 1000;
+    n = size(A,2);
+    Q = Matrix{FLOAT}[];
+    Qlock = Matrix{FLOAT}[];
+    Qi = randn(DOUBLE,n,b);
+    Qi = Matrix{FLOAT}(qr(A*Qi).Q);
+   
+    D,V = lanczos_iteration(A,k,b,max_kryl_sz,Qi,Q,Qlock);
+    println("D1: $(D[end:-1:end-k+1])");
+    V = recover_eigvec(Q,Matrix{FLOAT}(V[:,end:-1:end-k+1]),k);
+    
+    return D,V;
 end
 
-BLAS.set_num_threads(1);
-# CUDA.math_mode!(CUDA.DEFAULT_MATH);
-to = TimerOutput();
-# d,_ = RBL(sprandn(DOUBLE,50,50,0.5),1,10);
-# d,_ = RBL_gpu(sprandn(DOUBLE,50,50,0.5),1,10);
-# to = TimerOutput();
-# bench();
-# show(to);
-# println();
+function RBL_restarted(A::Union{SparseMatrixCSC{DOUBLE}},k::Int64,b::Int64,step::Int64)
+#=
+Input parameters
+A - n by n Real Symmetric Matrix whose eigenvalues we seek
+k - number of largest eigenvalues
+b - block size
+
+Output parameters
+D - k by 1 vector with k largest eigenvalues of A (by magnitude)
+V - n by k matrix with eigenvectors associated with the k largest eigenvalues of A
+
+This routine uses the randomized block Lanczos algorithm to compute the k 
+largest eigenvalues of a matrix A.
+=#
+    if (mod(k,step) != 0)
+        throw(ArgumentError("number of desired eigenvalues should be multiple of step size"))
+    end
+
+    if (mod(step,b) != 0)
+        throw(ArgumentError("step size should be multiple of block size"))
+    end
+
+    max_kryl_sz = 1000;
+    n = size(A,2);
+    D = zeros(FLOAT);
+    
+    Q = Matrix{FLOAT}[];
+    Qlock = Matrix{FLOAT}[];
+    Qi = randn(DOUBLE,n,b);
+    Qi = Matrix{FLOAT}(qr(A*Qi).Q);
+    
+    count = 0;
+    while (count < k)
+        D,V = lanczos_iteration(A,step,b,max_kryl_sz,Qi,Q,Qlock);
+        println("D1: $(D[end:-1:end-step+1])");
+        QV = recover_eigvec(Q,Matrix{FLOAT}(V[:,end:-1:end-step+1]),step);
+        
+        Qi = recover_eigvec(Q,Matrix{FLOAT}(V[:,1:b]),b);
+        Q = Matrix{FLOAT}[];
+        blks = Int64(step/b);
+        for i=1:blks
+            push!(Qlock,QV[:,(i-1)*b+1:i*b]);
+        end
+        count = count + step;
+    end
+    
+    V = Matrix{FLOAT}(undef,n,k);
+    for i=1:length(Qlock)
+        V[:,(i-1)*b+1:i*b] = Qlock[i];
+    end
+
+    return D,V;
+end
