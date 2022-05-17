37410b17d26a4b30305447b91403edb4d55a3a6c
iasonas <iasonaspav@gmail.com>
2022-05-18 01:02:00 +0300

    Move restarted implementation in different file

--- Julia/RBL.jl
+++ Julia/RBL.jl
@@ -115,55 +115,6 @@ function lanczos_iteration(A::Union{SparseMatrixCSC{DOUBLE},Matrix{DOUBLE}},k::I
     return D[end:-1:end-k+1],V[:,end:-1:end-k+1];
 end
 
-function new_lanczos_iteration(
-    A::Union{SparseMatrixCSC{DOUBLE},Matrix{DOUBLE}},b::Int64,kryl_sz::Int64,Qi::Matrix{FLOAT},
-    Q::Vector{Matrix{FLOAT}},Qlock::Vector{Matrix{FLOAT}}
-)
-    D = zeros(DOUBLE);
-    V = zeros(DOUBLE);
-    B = Matrix{DOUBLE}[];
-    
-    # first loop
-    restart_reorth!(Qlock,Qi);
-    push!(Q,Qi);
-    @timeit to "A*Qi" U::Matrix{DOUBLE} = A*Qi;
-    Ai::Matrix{DOUBLE} = transpose(Qi)*U;
-    mul!(U,Qi,Ai,-1.0,1.0);
-    U = Matrix{FLOAT}(U);
-    fact = qr(U);
-    Qi = Matrix{FLOAT}(fact.Q);
-    Bi = Matrix{DOUBLE}(fact.R);
-    T = insertA!(Ai,b);
-    insertB!(Bi,T,b,1);
-    i = 2;
-    while i*b < kryl_sz
-        push!(Q,Qi);
-        if mod(i,3) == 0
-            part_reorth!(length(Qlock),Qlock,Q[i],Q[i-1]);
-            @timeit to "part_reorth" part_reorth!(Q);
-        end
-        @timeit to "loc_reorth" loc_reorth!(Q[i],Q[i-1]);
-        @timeit to "A*Qi" mul!(U,A,Q[i]);
-        @timeit to "A*Qi" mul!(U,Q[i-1],transpose(Bi),-1.0,1.0);  # U = A*Q[i] - Q[i-1]*transpose(Bi)
-        @timeit to "Ai" mul!(Ai,transpose(Q[i]),U,1.0,0.0);
-        U = Matrix{FLOAT}(U);
-        @timeit to "U-QiAi" mul!(U,Qi,Ai,-1.0,1.0);
-        @timeit to "qr" fact = qr(U);
-        @timeit to "qr" Qi = Matrix{FLOAT}(fact.Q);
-        Bi = Matrix{DOUBLE}(fact.R);
-        T = [T insertA!(Ai,b)];
-        insertB!(Bi,T,b,i);
-        i = i + 1;
-    end
-    part_reorth!(length(Qlock),Qlock,Q[end],Q[end-1]);
-    @timeit to "part_reorth" part_reorth!(Q);
-    println("Iterations: $i");
-    @timeit to "dsbev" D,V = dsbev('V','L',T);
-    res_bounds = Bi*V[end-b+1:end,end:-1:1]; # residual bounds in descending order
-    return D[end:-1:1],V[:,end:-1:1],res_bounds;
-end
-
-
 function RBL(A::Union{SparseMatrixCSC{DOUBLE},Matrix{DOUBLE}},k::Int64,b::Int64)
 #=
 Input parameters
@@ -185,58 +136,6 @@ largest eigenvalues of a matrix A.
     Qi = Matrix{FLOAT}(qr(A*Qi).Q);
    
     D,V = lanczos_iteration(A,k,b,max_kryl_sz,Qi,Q);
-    V = recover_eigvec(Q,Matrix{FLOAT}(V),k);
-    return D,V;
-end
-
-function RBL_restarted(A::Union{SparseMatrixCSC{DOUBLE},Matrix{DOUBLE}},k::Int64)
-#=
-Input parameters
-A - n by n Real Symmetric Matrix whose eigenvalues we seek
-k - number of largest eigenvalues
-b - block size
-
-Output parameters
-D - k by 1 vector with k largest eigenvalues of A (by magnitude)
-V - n by k matrix with eigenvectors associated with the k largest eigenvalues of A
-
-This routine uses the randomized block Lanczos algorithm to compute the k 
-largest eigenvalues of a matrix A.
-=#
-    max_kryl_sz = 80;
-    n = size(A,2);
-    D = zeros(FLOAT,k,1);
-    V = zeros(FLOAT,n,k);
-    
-    Q = Matrix{FLOAT}[];
-    Qlock = Matrix{FLOAT}[];
-    Qi = randn(DOUBLE,n,1);
-    Qi = Matrix{FLOAT}(qr(A*Qi).Q);
-    
-    count = 0;
-    while (count < k)
-        d,v,conv = new_lanczos_iteration(A,1,max_kryl_sz,Qi,Q,Qlock);
-        ncomp = 0;
-        for i=1:length(conv)
-            if (count + ncomp < k)
-                if norm(conv[i]) < 1e-7
-                    ncomp = ncomp + 1;
-                    println("val: $(d[i])");
-                    @timeit to "recovery" qv = recover_eigvec(Q,Matrix{FLOAT}(v[:,i:i]),1);
-                    push!(Qlock,qv);
-                    D[count+ncomp] = d[i];
-                else
-                    @timeit to "recovery" Qi = recover_eigvec(Q,Matrix{FLOAT}(v[:,i:i]),1);
-                    break;
-                end
-            else
-                break;
-            end
-        end
-        
-        Q = Matrix{FLOAT}[];
-        count = count + ncomp;
-        max_kryl_sz = max_kryl_sz + 10;
-    end
+    @timeit to "Ritz vectors" V = recover_eigvec(Q,Matrix{FLOAT}(V),k);
     return D,V;
 end
